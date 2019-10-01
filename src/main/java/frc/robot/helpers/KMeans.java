903de6915040bba3fb58bc8cb27a5849895ec950
Tobias Alam <tobiasalam@gmail.com>
2019-10-01 16:56:28 -0400

    finish KMeans (theoretical)

--- src/main/java/frc/robot/helpers/KMeans.java
+++ src/main/java/frc/robot/helpers/KMeans.java
@@ -6,12 +6,17 @@ import java.util.Random;
 public class KMeans {
     private static Random generator = new Random();
 
-    public static ArrayList<ArrayList<Point>> returnClusters(ArrayList<Point> points, int k) {
+    public static ArrayList<ArrayList<Point>> returnClusters(ArrayList<Point> points, int k, int maximumThreshold) {
         ArrayList<ArrayList<Point>> clusters = new ArrayList<ArrayList<Point>>();
         int numOfPoints = points.size();
         ArrayList<Point> kValues = getKValues(points, numOfPoints, k);
         for (int i = 0; i < kValues.size(); i++) {
-            
+            ArrayList<Point> cluster = new ArrayList<Point>();
+            cluster.add(kValues.get(i));
+            for (int j = 0; j < numOfPoints; j++) {
+                if (Geo.getDistance(kValues.get(i), points.get(j)) <= maximumThreshold) {
+                    cluster.add(points.get(j));
+            }
         }
         return clusters;
     }
